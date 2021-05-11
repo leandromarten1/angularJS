ccb1ecfcb6f3d582793a0ed7dabd336e14ac2082
leandromarten1 <leandromarten1@gmail.com>
2021-05-11 15:11:25 -0300

    style guide todoController

--- js/controllers/todoController.js
+++ js/controllers/todoController.js
@@ -1,19 +1,48 @@
-angular
-  .module('ToDoList')
-  .controller('todoController', ($scope, todoFactory) => {
-    $scope.message = 'My app todo list';
-    $scope.todos = todoFactory.getTodos();
-    $scope.add = (todo) => {
+// angular
+//   .module('ToDoList')
+//   .controller('todoController', ($scope, todoFactory) => {
+//     console.log($scope);
+//     $scope.message = 'My app todo list';
+//     $scope.todos = todoFactory.getTodos();
+//     $scope.add = (todo) => {
+//       todo.done = false;
+//       todoFactory.addTodo(angular.copy(todo));
+//       delete $scope.todo;
+//     };
+
+//     $scope.removeTodo = (todo) => {
+//       $scope.todos = todoFactory.removeThisTodo(todo);
+//     };
+
+//     $scope.removeDone = (todos) => {
+//       $scope.todos = todoFactory.removeAllTodosDone(todos);
+//     };
+//   });
+
+(function () {
+  'use strict';
+
+  angular.module('ToDoList').controller('todoController', todoController);
+
+  todoController.$inject = ['todoFactory'];
+
+  function todoController(todoFactory) {
+    var vm = this;
+
+    vm.message = 'My app todo list';
+    vm.todos = todoFactory.getTodos();
+    vm.add = (todo) => {
       todo.done = false;
       todoFactory.addTodo(angular.copy(todo));
-      delete $scope.todo;
+      delete vm.todo;
     };
 
-    $scope.removeTodo = (todo) => {
-      $scope.todos = todoFactory.removeThisTodo(todo);
+    vm.removeTodo = (todo) => {
+      vm.todos = todoFactory.removeThisTodo(todo);
     };
 
-    $scope.removeDone = (todos) => {
-      $scope.todos = todoFactory.removeAllTodosDone(todos);
+    vm.removeDone = (todos) => {
+      vm.todos = todoFactory.removeAllTodosDone(todos);
     };
-  });
+  }
+})();
