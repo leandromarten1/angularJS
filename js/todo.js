6fd47f834ac29fd308424eb5705cac8417f5e39d
leandromarten1 <leandromarten1@gmail.com>
2021-05-11 10:42:07 -0300

    adicionando ui.router

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,18 @@
-angular.module('ToDoList', []);
+angular.module('ToDoList', ['ui.router']);
+
+angular.module('ToDoList').config(function ($stateProvider) {
+  let helloState = {
+    name: 'app',
+    url: '/',
+    templateUrl: 'view/todo.html',
+  };
+
+  let aboutState = {
+    name: 'about',
+    url: '/about',
+    templateUrl: 'view/about.html',
+  };
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});
